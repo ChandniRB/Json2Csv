b2e7e768fcb6b643ecdf2ad5ee595ab33432414e
Chandni <chandni15rb@gmail.com>
2022-08-11 12:09:24 +0530

    Changed HEADER value

--- src/main/java/json2csv/Constants.java
+++ src/main/java/json2csv/Constants.java
@@ -2,7 +2,7 @@ package json2csv;
 public class Constants {
     static String JSON_FILE="path to input JSON file";
     static String CSV_FILE="path to output CSV file";
-    static String[] HEADER={"Position", "Activity", "Count"};
+    static String[] HEADER={"Header1", "Header2", "Header3"};
     static String ROOT_ELEMENT="key";
     static String COUNT_FIELD = "doc_count";
 }
